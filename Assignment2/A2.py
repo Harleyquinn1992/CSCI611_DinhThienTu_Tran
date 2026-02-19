cc37f76b836f7110dcb61ea26d5bb7fc6b0bd875
DinhThienTuTran <kevinthientran2323@gmail.com>
2026-02-18 21:45:56 -0800

    Update codes with detailed steps. Picture of the first trainning result

---
+++ Assignment2/A2.py
@@ -0,0 +1,271 @@
+import torch
+import numpy as np
+
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+import matplotlib
+matplotlib.use('TkAgg')  # or Qt5Agg
+import matplotlib.pyplot as plt
+#For CNN
+import torch.nn as nn
+import torch.nn.functional as F 
+
+# -----------------------
+# 1.
+# LOAD AND NORMALIZE DATA
+# -----------------------
+
+#check for CUDA
+train_on_gpu = torch.cuda.is_available()
+
+# number of subprocesses to use for data loading
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+# percentage of training set to use as validation
+valid_size = 0.2
+
+# convert data to a normalized torch.FloatTensor
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+# choose the training and test datasets
+train_data = datasets.CIFAR10('data', train=True,
+                              download=True, transform=transform)
+test_data = datasets.CIFAR10('data', train=False,
+                             download=True, transform=transform)
+
+# obtain training indices that will be used for validation
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.shuffle(indices)
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+# define samplers for obtaining training and validation batches
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+# prepare data loaders (combine dataset and sampler)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+    sampler=train_sampler, num_workers=num_workers)
+valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+    sampler=valid_sampler, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
+    num_workers=num_workers)
+
+# specify the image classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+
+# obtain one batch of training images
+dataiter = iter(train_loader)
+#images, labels = dataiter.next() #python, torchvision version match issue
+images, labels = next(dataiter)
+images = images.numpy() # convert images to numpy for display
+
+# show training images
+rgb_img = np.squeeze(images[3])
+channels = ['red channel', 'green channel', 'blue channel']
+
+fig = plt.figure(figsize = (25, 18)) 
+for idx in np.arange(rgb_img.shape[0]):
+    ax = fig.add_subplot(1, 3, idx + 1)
+    img = rgb_img[idx]
+    ax.imshow(img, cmap='gray')
+    ax.set_title(channels[idx])
+    width, height = img.shape
+    thresh = img.max()/2.5
+    for x in range(width):
+        for y in range(height):
+            val = round(img[x][y],2) if img[x][y] !=0 else 0
+            ax.annotate(str(val), xy=(y,x),
+                    horizontalalignment='center',
+                    verticalalignment='center', size=8,
+                    color='white' if img[x][y]<thresh else 'black')
+            
+# plt.tight_layout()
+# plt.show()
+
+# -------------------------
+# 2.
+# DEFINE A CNN ARCHITECTURE
+# -------------------------
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        # TOTO: Build multiple convolutional layers (sees 32x32x3 image tensor in the first hidden layer)
+        # for example, conv1, conv2 and conv3
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1) #kernel_size?
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1) #kernel_size?
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1) #kernel_size?
+        
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # TODO: Build some linear layers (fully connected)
+        # for example, fc1 and fc2
+        self.fc1 = nn.Linear(32*8*8, 256)
+        self.fc2 = nn.Linear(256,10)
+    
+        # TODO: dropout layer (p=0.25, you can adjust)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        # assume we have 2 convolutional layers defined agove
+        # and we do a maxpooling after each conv layer
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        # TODO: flatten x at this point to get it ready to feed into the fully connected layer(s)
+        # Can use this but need to figure out the actual value for a, b and c
+        # x = x.view(-1, 128*8*8)
+        x = x.view(x.size(0), -1)
+        # optional add dropout layer
+        x = self.dropout(x)
+        
+        # add 1st hidden layer, with relu activation function
+        x = F.relu(self.fc1(x))
+        # optional add dropout layer
+        x = self.dropout(x)
+        # add 2nd hidden layer, with relu activation function
+        x = self.fc2(x)
+        return x
+
+# create a complete CNN
+model = Net()
+print(model)
+
+# move tensors to GPU if CUDA is available
+if train_on_gpu:
+    model.cuda()
+
+# ----------------------------------
+# 3
+# DEFINE LOSS FUNCTION AND OPTIMIZER
+# ----------------------------------
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# -------------
+# 4
+# TRAIN NETWORK
+# -------------
+# number of epochs to train the model, you decide the number
+n_epochs = 5
+
+valid_loss_min = np.inf # track change in validation loss
+
+for epoch in range(1, n_epochs+1):
+
+    # keep track of training and validation loss
+    train_loss = 0.0
+    valid_loss = 0.0
+    
+    ###################
+    # train the model #
+    ###################
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu: # to check for CUDA
+            data, target = data.cuda(), target.cuda()
+        #  clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update training loss
+        train_loss += loss.item()*data.size(0)
+        
+    ######################    
+    # validate the model #
+    ######################
+    model.eval()
+    for batch_idx, (data, target) in enumerate(valid_loader):
+        # move tensors to GPU if CUDA is available
+        # if train_on_gpu: #remember indentation for the line after this if decided to use it.
+        data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # update average validation loss 
+        valid_loss += loss.item()*data.size(0)
+    
+    # calculate average losses
+    train_loss = train_loss/len(train_loader.sampler)
+    valid_loss = valid_loss/len(valid_loader.sampler)
+        
+    # print training/validation statistics 
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+        epoch, train_loss, valid_loss))
+    
+    # save model if validation loss has decreased
+    if valid_loss <= valid_loss_min:
+        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+        valid_loss_min,
+        valid_loss))
+        torch.save(model.state_dict(), 'model_trained.pt')
+        valid_loss_min = valid_loss
+
+print('Finished Training')
+
+model.load_state_dict(torch.load('model_trained.pt'))
+
+# track test loss
+test_loss = 0.0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+model.eval()
+# iterate over test data
+for batch_idx, (data, target) in enumerate(test_loader):
+    # move tensors to GPU if CUDA is available
+    if train_on_gpu:
+        data, target = data.cuda(), target.cuda()
+    # forward pass: compute predicted outputs by passing inputs to the model
+    output = model(data)
+    # calculate the batch loss
+    loss = criterion(output, target)
+    # update test loss 
+    test_loss += loss.item()*data.size(0)
+    # convert output probabilities to predicted class
+    _, pred = torch.max(output, 1)    
+    # compare predictions to true label
+    correct_tensor = pred.eq(target.data.view_as(pred))
+    correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+    # calculate test accuracy for each object class
+    for i in range(batch_size):
+        label = target.data[i]
+        class_correct[label] += correct[i].item()
+        class_total[label] += 1
+
+# average test loss
+test_loss = test_loss/len(test_loader.dataset)
+print('Test Loss: {:.6f}\n'.format(test_loss))
+
+for i in range(10):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+            classes[i], 100 * class_correct[i] / class_total[i],
+            np.sum(class_correct[i]), np.sum(class_total[i])))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total),
+    np.sum(class_correct), np.sum(class_total)))
