02a884bc4800f075fea5412557aebbe26d647d27
Dinh Thien Tu Tran <87918128+Harleyquinn1992@users.noreply.github.com>
2026-03-13 00:35:28 -0700

    Delete Assignment3/PretrainedYOLO.py

--- Assignment3/PretrainedYOLO.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-
-model = YOLO("yolov8n.pt") # Load pre-trained YOLO model
-
-results = model("test.png", conf = 0.5)
-annotated = results[0].plot()
-cv2.imwrite("test_result.png", annotated)
